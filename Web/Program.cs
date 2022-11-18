8f6d21dc2f91e70d0a321e6bd4d74aeeba62e525
Marcel Diepold <marcel.diepold@bmw.de>
2022-11-18 17:36:33 +0100

    Add https middleware

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
